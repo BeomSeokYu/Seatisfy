fbc0bb41fb3b1ab86a1736202949c8f44bdc4f44
BeomSeok Yu <69231700+BeomSeokYu@users.noreply.github.com>
2023-04-12 15:49:03 +0900

    Update HomeController.java

--- src/main/java/com/reserve/seat/HomeController.java
+++ src/main/java/com/reserve/seat/HomeController.java
@@ -9,17 +9,23 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.reserve.seat.HomeController;
+import com.reserve.seat.user.User;
 import com.reserve.seat.user.UserService;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * Handles requests for the application home page.
  */
+
+@Slf4j
 @Controller
 @RequiredArgsConstructor
 public class HomeController {
@@ -45,8 +51,10 @@ public class HomeController {
 	
 	@ResponseBody
 	@PostMapping("/uname")
-	public String getUserName(String username) {
-		return userService.getUserDetail(username).getName();
+	public User getUserName(@ModelAttribute User user) {
+		user.setName(userService.getUserDetail(user.getUsername()).getName());
+		log.info("{}", user.getName());
+		return user;
 	}
 	
 }
